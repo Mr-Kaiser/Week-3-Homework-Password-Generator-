9481a6a2ff1fd2636394988e781048a68c8c8cc6
Laptop <jimmy.hampton@outlook.com>
2022-07-06 18:53:52 -0600

    Bacon

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function prompts(){
+  var numberofcharacters;
+  var Charactersprompt = prompt('How many characters?','8-128');
+    if (Charactersprompt > 8 && Charactersprompt < 128) {
+      numberofcharacters = Charactersprompt
+    } else {
+     alert ('invalidinput')
+     return prompts()
+    } ;
+  var uppercaseprompt = confirm ('Include Uppercase Letters?\n Ok=yes, cancel=no');
+  var lowercaseprompt = confirm ('Include Lowercase Letters?\n Ok=yes, cancel=no');
+  var Numbersprompt = confirm ('Include Numbers?\n Ok=yes, cancel=no');
+  var specialprompt = confirm ('Include Special Characters?\n Ok=yes, cancel=no'); 
+}
+
+uppercasenum = (math.floor(math.random()* 26)+65)
+console.log uppercasenum;
+
+lowercasenum = (math.floor(math.random()*26) + 97)
+numberofnums = (math.floor(math.random()*10)+48)
+
+
+
+
+
+var finalpass = []
+  while (finalpass.length < numberofcharacters){
+    finalpass= uppercaseprompt+ lowercasenum +numberofcharacters)
+  }
+   
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -11,4 +43,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', prompts);
